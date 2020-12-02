0a9c801e2d3b8853e1cd0f878c4a7fd9e5a21ec2
Alan Grgic <alan.grgic@gmail.com>
2020-12-01 23:12:39 -0600

    reorganize day 1

--- 1_report_repair/partone.py
+++ 1_report_repair/partone.py
@@ -1,14 +1,17 @@
 from os import path
 
 this_dir = path.dirname(path.realpath(__file__))
-input_file = path.join(this_dir, "inputs", "input.txt")
+input_file = path.join(this_dir, "input.txt")
+
+# Prompt: get the product of the two numbers in the input
+# that add up to 2020
 
 checked = set()
 with open(input_file) as f:
-    for line in f:
-        line = int(line.strip())
-        diff = 2020 - line
+    for entry in f:
+        entry = int(entry.strip())
+        diff = 2020 - entry
         if diff in checked:
-            print(f"{line} * {diff} = {line * diff}")
+            print(f"{entry} * {diff} = {entry * diff}")
             exit()
-        checked.add(line)
+        checked.add(entry)
--- 1_report_repair/parttwo.py
+++ 1_report_repair/parttwo.py
@@ -1,16 +1,18 @@
 from os import path
 
 this_dir = path.dirname(path.realpath(__file__))
-input_file = path.join(this_dir, "inputs", "input.txt")
+input_file = path.join(this_dir, "input.txt")
+
+# Prompt: get the product of the three numbers in the input
+# that add up to 2020
 
 checked = set()
 with open(input_file) as f:
-    for line in f:
-        line = int(line.strip())
-
+    for entry in f:
+        entry = int(entry.strip())
         for item in checked:
-            prev = 2020 - (line + item)
+            prev = 2020 - (entry + item)
             if prev in checked:
-                print(f"{line} * {item} * {prev} = {line * item * prev}")
+                print(f"{entry} * {item} * {prev} = {entry * item * prev}")
                 exit()
-        checked.add(line)
+        checked.add(entry)
