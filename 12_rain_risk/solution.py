6b44b684203aa4d32cfff0c4fcb6fddabbef6f8f
Alan Grgic <alan.grgic@gmail.com>
2021-06-17 00:15:31 -0500

    refactor

--- 12_rain_risk/solution.py
+++ 12_rain_risk/solution.py
@@ -6,6 +6,35 @@ input_file = path.join(this_dir, "input.txt")
 MOVES = ["W", "N", "E", "S"]
 TURNS = ["L", "R"]
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self, direction, amount):
+        if direction == "E":
+            self.x += int(amount)
+        elif direction == "N":
+            self.y += int(amount)
+        elif direction == "W":
+            self.x -= int(amount)
+        elif direction == "S":
+            self.y -= int(amount)
+
+    def rotate(self, direction, amount):
+        rotations = int(amount) // 90
+
+        if direction == "L":
+            rotations = (rotations * -1) % 4
+
+        for _ in range(rotations):
+            x = self.x
+            y = self.y
+
+            self.x = y
+            self.y = x * -1
+
+
 class Navigator:
     def __init__(self, movement_handler):
         self._movement_handler = movement_handler
@@ -22,68 +51,43 @@ class Navigator:
 class ShipMovementHandler:
 
     def __init__(self):
-        self._ns_pos = 0
-        self._ew_pos = 0
+        self._ship_loc = Point(0, 0)
         self._facing_idx = 2
 
     def get_manhattan_distance(self):
-        return (abs(self._ew_pos) + abs(self._ns_pos))
+        return (abs(self._ship_loc.x) + abs(self._ship_loc.y))
 
     def turn(self, action, amount):
+        offset = int(amount) // 90
         if action == "L":
-            self._facing_idx = (self._facing_idx - (int(amount) // 90)) % 4
-        elif action == "R":
-            self._facing_idx = (self._facing_idx + (int(amount) // 90)) % 4
+            offset *= -1
+        self._facing_idx = (self._facing_idx + offset) % 4
 
     def move(self, action, amount):
         if action == "F":
             self.move(MOVES[self._facing_idx], amount)
-        elif action == "E":
-            self._ew_pos += int(amount)
-        elif action == "N":
-            self._ns_pos += int(amount)
-        elif action == "W":
-            self._ew_pos -= int(amount)
-        elif action == "S":
-            self._ns_pos -= int(amount)
+        else:
+            self._ship_loc.move(action, amount)
 
 
 class WaypointMovementHandler:
 
     def __init__(self):
-        self._wp_ns_pos = 1
-        self._wp_ew_pos = 10
-        self._ns_pos = 0
-        self._ew_pos = 0
+        self._waypoint = Point(10, 1)
+        self._ship_loc = Point(0, 0)
 
     def get_manhattan_distance(self):
-        return (abs(self._ew_pos) + abs(self._ns_pos))
+        return (abs(self._ship_loc.x) + abs(self._ship_loc.y))
 
     def turn(self, action, amount):
-        rotations = int(amount) // 90
-
-        if action == "L":
-            rotations = (rotations * -1) % 4
-
-        for _ in range(rotations):
-            wp_ew_pos = self._wp_ew_pos
-            wp_ns_pos = self._wp_ns_pos
-
-            self._wp_ew_pos = wp_ns_pos
-            self._wp_ns_pos = wp_ew_pos * -1
+        self._waypoint.rotate(action, amount)
 
     def move(self, action, amount):
         if action == "F":
-            self._ns_pos += (self._wp_ns_pos * int(amount))
-            self._ew_pos += (self._wp_ew_pos * int(amount))
-        elif action == "E":
-            self._wp_ew_pos += int(amount)
-        elif action == "N":
-            self._wp_ns_pos += int(amount)
-        elif action == "W":
-            self._wp_ew_pos -= int(amount)
-        elif action == "S":
-            self._wp_ns_pos -= int(amount)
+            self._ship_loc.y += (self._waypoint.y * int(amount))
+            self._ship_loc.x += (self._waypoint.x * int(amount))
+        else:
+            self._waypoint.move(action, amount)
 
 
 with open(input_file) as f:
@@ -95,5 +99,5 @@ with open(input_file) as f:
         amount = command[1:]
         ship_nav.handle_command(action, amount)
         waypoint_nav.handle_command(action, amount)
-    print(ship_nav.get_manhattan_distance())
-    print(waypoint_nav.get_manhattan_distance())
+    print(f"Part one: {ship_nav.get_manhattan_distance()}")
+    print(f"Part two: {waypoint_nav.get_manhattan_distance()}")
