5369d7a767ec031f16cb5ca717636a30357735db
Alan Grgic <alan.grgic@gmail.com>
2021-06-16 23:10:19 -0500

    day 12 pt 2 working

--- 12_rain_risk/solution.py
+++ 12_rain_risk/solution.py
@@ -7,29 +7,37 @@ MOVES = ["W", "N", "E", "S"]
 TURNS = ["L", "R"]
 
 class Navigator:
+    def __init__(self, movement_handler):
+        self._movement_handler = movement_handler
+
+    def get_manhattan_distance(self):
+        return self._movement_handler.get_manhattan_distance()
+
+    def handle_command(self, action, amount):
+        if action in TURNS:
+            self._movement_handler.turn(action, amount)
+        else:
+            self._movement_handler.move(action, amount)
+
+class ShipMovementHandler:
+
     def __init__(self):
         self._ns_pos = 0
         self._ew_pos = 0
         self._facing_idx = 2
 
-    def handle_command(self, action, amount):
-        if action in TURNS:
-            self._turn(action, amount)
-        else:
-            self._move(action, amount)
-
     def get_manhattan_distance(self):
         return (abs(self._ew_pos) + abs(self._ns_pos))
 
-    def _turn(self, action, amount):
+    def turn(self, action, amount):
         if action == "L":
             self._facing_idx = (self._facing_idx - (int(amount) // 90)) % 4
         elif action == "R":
             self._facing_idx = (self._facing_idx + (int(amount) // 90)) % 4
 
-    def _move(self, action, amount):
+    def move(self, action, amount):
         if action == "F":
-            self._move(MOVES[self._facing_idx], amount)
+            self.move(MOVES[self._facing_idx], amount)
         elif action == "E":
             self._ew_pos += int(amount)
         elif action == "N":
@@ -40,12 +48,52 @@ class Navigator:
             self._ns_pos -= int(amount)
 
 
+class WaypointMovementHandler:
+
+    def __init__(self):
+        self._wp_ns_pos = 1
+        self._wp_ew_pos = 10
+        self._ns_pos = 0
+        self._ew_pos = 0
+
+    def get_manhattan_distance(self):
+        return (abs(self._ew_pos) + abs(self._ns_pos))
+
+    def turn(self, action, amount):
+        rotations = int(amount) // 90
+
+        if action == "L":
+            rotations = (rotations * -1) % 4
+
+        for _ in range(rotations):
+            wp_ew_pos = self._wp_ew_pos
+            wp_ns_pos = self._wp_ns_pos
+
+            self._wp_ew_pos = wp_ns_pos
+            self._wp_ns_pos = wp_ew_pos * -1
+
+    def move(self, action, amount):
+        if action == "F":
+            self._ns_pos += (self._wp_ns_pos * int(amount))
+            self._ew_pos += (self._wp_ew_pos * int(amount))
+        elif action == "E":
+            self._wp_ew_pos += int(amount)
+        elif action == "N":
+            self._wp_ns_pos += int(amount)
+        elif action == "W":
+            self._wp_ew_pos -= int(amount)
+        elif action == "S":
+            self._wp_ns_pos -= int(amount)
+
+
 with open(input_file) as f:
-    facing_idx = 0
     input_list = [entry.strip() for entry in f]
-    nav = Navigator()
+    ship_nav = Navigator(ShipMovementHandler())
+    waypoint_nav = Navigator(WaypointMovementHandler())
     for command in input_list:
         action = command[0]
         amount = command[1:]
-        nav.handle_command(action, amount)
-    print(nav.get_manhattan_distance())
+        ship_nav.handle_command(action, amount)
+        waypoint_nav.handle_command(action, amount)
+    print(ship_nav.get_manhattan_distance())
+    print(waypoint_nav.get_manhattan_distance())
